981019b589e259ff4b2207c6686519d62e70517d
Fabiano Batista da Silva <fabianobatista@outlook.com>
2023-03-15 14:43:37 -0300

    add class App, Curso and Mentoria

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,35 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataMentoria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataMentoria() {
+        return dataMentoria;
+    }
+
+    public void setDataMentoria(LocalDate dataMentoria) {
+        this.dataMentoria = dataMentoria;
+    }
+
+    public void calcularXp() {}
+}
